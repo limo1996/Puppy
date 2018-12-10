7b54e62ba7c38557a30872e9daa90eba46f9daae
Jakub Lichman <lichman.jakub@gmail.com>
2018-12-10 14:32:15 +0100

    Bunch of interesting tests added.

--- test/1/Test.java
+++ test/1/Test.java
@@ -1,10 +1,42 @@
 public class Test {	
 	public static void main(String[] args) {	
 		Test t = new Test();
+		t.ex1(0,0,1);
+		t.ex2(0);
+		t.ex3(1);
+		t.ex4(8);
+		t.ex5(8);
+		t.ex6(0,0);
+		t.ex7(0);
+		t.ex8(-5,7,6);
+		t.ex9(1,1,0);
+		t.ex10(10,0,10);
+		t.ex11(0,1,0);
+		t.ex12(1,0,0);
+		t.ex13(-3,-1,1);
+		t.ex14(0);
+		t.ex15(1,1,1);
+		t.ex16(0,0,2);
+		t.ex17(4,2,3);
 		t.ex18(0,8,10);
+		t.ex19(2,3,2);
+		t.ex20(4,2,3);
+		t.curly(0,0,1);
+		t.swapping(0,0);
+		t.bright(0,0,5,-4,-1,4,-1);
+		t.div(0);
+		t.lon(0);
+		t.javali1(-1,0,0,0);
+		t.javali2(3,2,2);
+		t.deepPure(0,0,0,0,7,0,0,0);
+		t.lol(3);
+		t.lol2(4);
+		t.lol3(4);
+		t.deep_mini(-1,-5,-8,-8,-4,-4);
+		t.deep(6,1,-6,2,1,1);
 	}
 
-    /*void ex1(int a, int b, int c) { // 0 0 1
+    void ex1(int a, int b, int c) { // 0 0 1
         int d;
         if(c == 0){
             d = a;
@@ -274,7 +306,7 @@ public class Test {
 
 		if(w + x + y + z> 10)
 			error("ex17");
-	}*/
+	}
 
 	void ex18(int a, int b, int c) {
 		int x = 0;
@@ -303,7 +335,7 @@ public class Test {
 			error("ex18");
 	}
 	
-	/*void ex19(int a, int b, int c) {
+	void ex19(int a, int b, int c) {
 		int x = 1, y = 2, z = 3, w = 4;
 		if(a == c)
 			x = 2;
@@ -329,9 +361,9 @@ public class Test {
 
 		if(x + y + z + w > 10)
 			error("ex19");
-	} */
+	}
 
-	/*void ex20(int a, int b, int c) {
+	void ex20(int a, int b, int c) {
 		int x = 1, y = 2, z = 3, w = 4;
 		if(a == c)
 			x = 2;
@@ -371,9 +403,227 @@ public class Test {
 		}
 		if(x+y+z==3)
 			error("curly");
-	}*/
+	}
+
+	void swapping(int a, int b) {
+		int x = a, y = b;
+		int tmp = x;
+		x = y;
+		y = tmp;
+		if(y == a && x == b)
+			error("swapping");
+	}
+
+	void bright(int a, int b, int c, int d, int e, int f, int g) {
+		int x = 0;
+		if(a * b + c - x + g == c - b * a + e) {
+			x = 1;
+		} else {
+			x = 3;
+		}
+
+		if(x * a - b == 0 && c - d * e < f + g && d * e - g == c && c + b > f) {
+			if(a == 0 || b == 0 || c == 0 || d == 0 || e == f + g || f == 3 || g == 11)
+				error("bright");
+		}
+	}
+
+	void div(int a) {
+		int x = a, y = 2*a;
+		if(a == (x + y) / 3)
+			error("div");
+	}
+
+	void lon(int a) {
+		int x = 8, y = 9, z = 0;
+		if(a == 0)
+			x++;
+		if(x == y) {
+			x++;
+			y--;
+		} else {
+			x++;
+			y++;
+		}
+
+		if(x - y == 2) {
+			z++;
+			y++;
+		} else {
+			z--;
+		}
+
+		if(z < 0)
+			return;
+
+		if(z > 0 && x - (a + y) == 1)
+			error("lon");
+	}
+
+	void javali1(int i1, int i2, int i3, int i4) {	//1 1 0 1
+		int x1 = -1, x2 = -1, x3 = -1, x4 = -1;
+		if(i1 == i2) {
+			x1 = 4;
+			if(i2 == i3) {
+				x2 = 4;
+				if(i1 == i3) {
+					x3 = 5;
+				}
+			} else {
+				x2 = 3;
+				if(i3 == i4) {
+					x3 = 1;
+				} else {
+					x3 = 2;
+					if(i1 == i4) {	
+						x4 = 1;
+					}
+				}
+			}
+		} else {
+			x1 = 4;
+			if(i2 == i3){
+				x2 = i3 + 1;
+			}
+		}
+
+		if(x1 * x2 * x3 * x4 > 0)
+			error("javali1");
+	} 
+
+	void javali2(int i1, int i2, int i3) {
+		int x1 = 0, x2 = 0, x3 = 0;
+		if(i1 >= i2) {
+			if(i2 == i1){
+				x3 = i2;
+			} else {
+				if(i2 == i3){
+					x2 = 3;
+				}
+				x1 = 2;
+			}
+		} else {
+			x3 = i1 + i1 * i2;
+		}
+
+		if(i3 == i2) {
+			x3 = i3 + i2;
+		}
+
+		if(x3 + x2 == 7 && x1 == 2)
+			error("javali2");
+	}
+
+	void deepPure(int a, int b, int c, int d, int e, int f, int g, int h) {
+		if(a == 0 && b == c && c == 2 * d + f) {
+			if(b == 0 || d == 0 || g == 0 || h == 9) {
+				if(e == 7 && c * e + g == h * a * f && h == 0) {
+					if(g == 0 || d == 0 || c == d || h == 9) {
+						if(a % 5 == 0)
+							error("deepPure");
+					}
+				}
+			}
+		}
+	}
+
+	void lol(int a) {
+		int x = 0, y = 0;
+		if(a % 9 == 0 && a % 5 == 0) {
+			x = 2 * a;
+			y = 9;
+		} else {
+			y = 2 * a;
+			x = 6;
+		}
+
+		if(y == x)
+			y = y + 5;
 		
-	
+		if(y > x)
+			error("lol");
+	}
+
+	void lol2(int a) {
+		int x = 0, y = 0;
+		if(a / 9 == 0 && a / 5 == 0) {
+			x = 2 * a;
+			y = 9;
+		} else {
+			y = 2 * a;
+			x = 6;
+		}
+
+		if(y == x)
+			y = y + 5;
+		
+		if(y > x)
+			error("lol2");
+	}
+
+	void lol3(int a) {
+		int x = 0, y = 0;
+		if(a / 9 == 0 && a / 5 == 0) {
+			x = 2 * a;
+			y = 9;
+		} else {
+			y = 2 * a;
+			x = 6;
+		}
+		
+		if(y > x)
+			error("lol3");
+	}
+
+	void deep_mini(int a, int b, int c, int d, int e, int f) {
+		int x = a * b + c;
+		int y = d - e;
+		int z = 0;
+		if(x != y){
+			int tmp = x;
+			x = y;
+			y = tmp;
+		}
+
+		if(x == f && y == a * b + c) {
+			if(x == y || f == e || b == a || c == a)
+				z = 9;
+			else 
+				z = 8;
+		}
+
+		if(z == 9) {
+			if(x != y)
+				error("deep_mini");
+		}
+	}
+
+	void deep(int a, int b, int c, int d, int e, int f) {
+		int x = a * b + c;
+		int y = d - e;
+		int z = 0;
+		if(x != y){
+			int tmp = x;
+			x = y;
+			y = tmp;
+		}
+
+		if(x == f && y == a * b + c) {
+			if(x == y || f == e || b == a || c == a)
+				z = 9;
+			else 
+				z = 8;
+		}
+
+		if(x != y && f == e + a && d == a && f == 2*f)
+			z = z + 1;
+
+		if(z == 9) {
+			if(x != y)
+				error("deep");
+		}
+	}
+		
 	void error(String msg) {
 		System.out.println("Error:" + msg);
 	}
@@ -382,4 +632,14 @@ public class Test {
 /**
  * TODO:
  * Find better strategy for picking of element to replace
+ * Implies has still duplicates on the left hand side  DONE
+ * If implication contains on the left expression that is clearly false just dont deal with it
+ * Duplicate implications (toString comparision is not a good idea since left side can be reordered)
+ * Merge implications by right hand side? INCORRECT
+ * Memoization?
+ * Propagate values to branches for instance if(a==0) in this branch we can assume that a = 0
+ * Enhance toProcess queue in following way:
+	 * create map [right side of implication] : left side
+	 * if implication to push has right hand side that is in the map just push left side to left side of map entry
+	 *  
  */
